934d5dbc0c77466e9c3d8e1e04fade3624191819
Christine Song <christinedisong@gmail.com>
2017-11-21 13:55:50 -0800

    Begin front end auth process Got text to render on localhost

---
+++ f00px/frontend/entryfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+// import Root from './components/Root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const store = configureStore(); 
+  const root = document.getElementById('root'); 
+  // ReactDOM.render(<Root store={store}/>, root);
+  ReactDOM.render(<h1>etc</h1>, root);
+
+});
+
+
