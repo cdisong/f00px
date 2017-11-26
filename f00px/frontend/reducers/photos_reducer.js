ef0897a03dc5fca431a3e8f10c5b295a62260d6b
Christine Song <christinedisong@gmail.com>
2017-11-25 20:50:23 -0800

    Add photos reducer, update root reducer

---
+++ f00px/frontend/reducers/photos_reducer.js
@@ -0,0 +1,23 @@
+import merge from 'lodash/merge'; 
+
+import { 
+  RECEIVE_SINGLE_PHOTO, 
+  DELETE_PHOTO, 
+  RECEIVE_ALL_PHOTOS 
+} from '../actions/photo_actions';
+
+const photosReducer = (state = {}, action) => {
+  Object.freeze(state); 
+  switch(action.type) { 
+    case RECEIVE_SINGLE_PHOTO: 
+      return merge({}, state, action.photo); 
+    case RECEIVE_ALL_PHOTOS: 
+      return merge({}, action.photos); 
+    case DELETE_PHOTO: 
+      let newState = merge({}, state); 
+      delete newState[action.id]; 
+      return newState;
+  }
+};
+
+export default photosReducer; 
--- f00px/frontend/reducers/root_reducer.js
+++ f00px/frontend/reducers/root_reducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 
 import session from './session_reducer';
 import errors from './errors_reducer';
-
+import photos from './photos_reducer';
 const rootReducer = combineReducers({
   session,
-  errors
+  errors, 
+  photos
 });
 
 
