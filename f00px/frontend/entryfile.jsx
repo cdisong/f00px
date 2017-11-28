e6aeb1be50aad0e81dd68e56ebfbec6df707779d
Christine Song <christinedisong@gmail.com>
2017-11-28 10:31:51 -0800

    Fix capitalization of Root entryfile

--- f00px/frontend/entryfile.jsx
+++ f00px/frontend/entryfile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { configureStore } from './store/store';
-import Root from './components/Root';
+import Root from './components/root';
 import { logIn, signUp, logOut } from './util/session_api_util'; 
 import { 
   fetchSinglePhoto, 
