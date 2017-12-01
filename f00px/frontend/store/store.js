9050d094f828529610e67c247d07af556028cac7
Christine Song <christinedisong@gmail.com>
2017-12-01 15:13:00 -0800

    production ready

--- f00px/frontend/store/store.js
+++ f00px/frontend/store/store.js
@@ -10,3 +10,11 @@ export const configureStore = (preloadedState = {}) => (
     applyMiddleware(thunk, logger)
   )
 );
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
