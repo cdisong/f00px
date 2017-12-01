268b62d38603e749bf870db9a69193e6cbe1c1df
Christine Song <christinedisong@gmail.com>
2017-12-01 13:24:06 -0800

    fix syntax follows controller

--- f00px/app/controllers/api/follows_controller.rb
+++ f00px/app/controllers/api/follows_controller.rb
@@ -6,7 +6,7 @@ class Api::FollowsController < ApplicationController
     if @follow.save 
       render "api/users/show"
     else 
-      render json: @follow.errors.full_messages, status 422
+      render json: @follow.errors.full_messages, status: 422
     end 
   end
 
