762156256f1573969e2bd75405759b68206b6cc6
Christine Song <christinedisong@gmail.com>
2018-04-29 02:20:37 -0700

    base styling complete

--- f00px/frontend/components/userprofile/user_profile.jsx
+++ f00px/frontend/components/userprofile/user_profile.jsx
@@ -21,6 +21,34 @@ const style = {
     right           : '100px',
     bottom          : '100px',
     margin          : 'auto',
+    width: '200px',
+    height: '186px',
+    // border          : '1px solid #sccc',
+    padding         : '15px',
+    zIndex          : 11
+
+    // opacity         : '',
+    // transition      : 'opacity 2s'
+  }
+};
+
+const styll = {
+  overlay : {
+    position        : 'fixed',
+    top             : 0,
+    left            : 0,
+    right           : 0,
+    bottom          : 0,
+    backgroundColor : 'rgba(255,255,255, 0.7)',
+    zIndex          : 10
+  },
+  content : {
+    position        : 'fixed',
+    top             : '100px',
+    left            : '100px',
+    right           : '100px',
+    bottom          : '100px',
+    margin          : 'auto',
     width           : '400px',
     height          : '400px',
     // border          : '1px solid #sccc',
@@ -32,6 +60,12 @@ const style = {
   }
 };
 
+const stylez = {
+  margin: '0px', 
+  height:  '125px', 
+  width: '192px',
+  border: '1px solid #0000004a'
+};
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -43,7 +77,8 @@ class UserProfile extends React.Component {
       modalOpen: false,
       followerModalOpen: false, 
       followingModalOpen: false,
-      photoDetailIsOpen: false
+      photoDetailIsOpen: false,
+      value: ""
     };
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.modalOpen = this.modalOpen.bind(this);
@@ -118,40 +153,42 @@ class UserProfile extends React.Component {
             <div className="image_user">
               <img src={this.state.user.profile_img_url}/>
             </div>
-              {this.state.user.username}
-            <br/>                
-            <br/>
-              {this.state.user.description}
-              <br/>
-              <br/>
-              <button onClick={this.modalOpen}>Update Description</button>
-              <Modal 
-                contentLabel="FollowerModal"
-                isOpen={this.state.modalOpen}
-                onRequestClose={this.modalClose}
-                shouldCloseOnOverlayClick={true}
-                ariaHideApp={false}
-                style={style}>
-              <form onSubmit={this.handleFormSubmit}>
-              <br/>
-              <section className="update">
-                <label> <section className="udescription"><u>Update Description</u></section>
+              <div className="user-info">
+                {this.state.user.username}
+                  <br/>                
                   <br/>
-                  <input type="text" 
-                    onChange={this.update('description')} 
-                    className="description-edit"
-                    placeholder="Description"
-                    />
-                </label>
-                <input type="submit" value="Update Description"/>
-                </section>
-              </form>
-              </Modal>
-              <div className="follower-button">
-              <button onClick={this.openFollowerModal}>Followers</button>
+                <div className="profile-modal-button">
+                  <button onClick={this.modalOpen}>(edit)</button>
+                </div>
+                {this.state.user.description}
               </div>
-              <div className="follower-button">
-              <button onClick={this.openFollowingModal}>Following</button>
+              <br/>
+              <br/>
+              <br/>
+              <div className="guh">
+                <div className="profile-modal-buttons">
+                  <button onClick={this.openFollowerModal}>{this.props.followers.length} Followers</button>
+                </div>
+                <div className="profile-modal-buttons">
+                  <button onClick={this.openFollowingModal}>{this.props.following.length} Following</button>
+                </div>
+                  <Modal 
+                    contentLabel="FollowerModal"
+                    isOpen={this.state.modalOpen}
+                    onRequestClose={this.modalClose}
+                    shouldCloseOnOverlayClick={true}
+                    ariaHideApp={false}
+                    style={style}>
+                  <form onSubmit={this.handleFormSubmit}>
+                  <div className="update">
+                    <label> <div className="udescription">Update Description</div>
+                    <br/>
+                      <textarea style={stylez} value={this.state.value} onChange={this.update('description')} />
+                    </label>
+                    <input type="submit" value="Save"/>
+                    </div>
+                  </form>
+                  </Modal>
               </div>
           </div>
           </section>
@@ -174,7 +211,7 @@ class UserProfile extends React.Component {
           onRequestClose={this.closeFollowerModal}
           shouldCloseOnOverlayClick={true}
           ariaHideApp={false}
-          style={style}>
+          style={styll}>
             <section className="followers-container">
             <strong className="follower-heading">Followed by {this.props.followers.length} users!</strong>
             <br/>
@@ -207,7 +244,7 @@ class UserProfile extends React.Component {
             onRequestClose={this.closeFollowingModal}
             shouldCloseOnOverlayClick={true}
             ariaHideApp={false}
-            style={style}>
+            style={styll}>
               <section className="followers-container">
               <strong className="follower-heading">Following {this.props.following.length} users!</strong>
               <br/>
