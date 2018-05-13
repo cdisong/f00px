87e504bb892c74277cfacb5ec0b6054aee04c244
Christine Song <christinedisong@gmail.com>
2018-05-12 22:24:43 -0700

    fix footer

--- f00px/frontend/components/splash/footer.jsx
+++ f00px/frontend/components/splash/footer.jsx
@@ -5,12 +5,13 @@ class Footer extends React.Component {
         return ( 
             <footer className="footer">
                 <section className="footer-left">
-                    <h1>ASDJKF</h1>
-                    <p>Hasjdfl;kasjdkfl;sajkldf;s a</p>
+                    <h1>F00PX</h1>
+                    <p>A photo-hosting web application inspired by 500px.</p> 
+                    <p>F00PX utilizes a PostgreSQL database, Ruby-on-Rails backend, and React.js/Redux frontend.</p>
                 </section>
                 <section className="footer-right">
                     <a href="https://github.com/cdisong">Github</a>
-                    <a href="https://www.linkedin.com/in/cdisong">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/cdisong">Linkedin</a>
                 </section>
             </footer>
         );
